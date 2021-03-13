7239c9266344b5fe0bce254dd135088f94b25de0
Giannis Stergiou <giannissterg@hotmail.com>
2021-03-13 23:14:27 +0200

    [client] Added more features to the client

--- chordy/chordy/controllers/base.py
+++ chordy/chordy/controllers/base.py
@@ -5,6 +5,7 @@ from ..core.version import get_version
 
 from ..generated import client_services_pb2
 from ..generated import client_services_pb2_grpc
+import grpc
 
 VERSION_BANNER = """
 CLI application that gives access to a distributed song database based on Chord protocol %s
@@ -37,49 +38,90 @@ class Base(Controller):
         self.app.args.print_help()
 
 
-    @ex(
-        help='example sub command1',
+    # @ex(
+    #     help='example sub command1',
 
+    #     # sub-command level arguments. ex: 'chordy command1 --foo bar'
+    #     arguments=[
+    #         ### add a sample foo option under subcommand namespace
+    #         ( [ '-f', '--foo' ],
+    #           { 'help' : 'notorious foo option',
+    #             'action'  : 'store',
+    #             'dest' : 'foo' } ),
+    #     ],
+    # )
+    # def show(self):
+    #     """Example sub-command."""
+
+    #     data = {
+    #         'foo' : 'bar',
+    #     }
+
+    #     ### do something with arguments
+    #     if self.app.pargs.foo is not None:
+    #         data['foo'] = self.app.pargs.foo
+
+    #     self.app.render(data, 'command1.jinja2')
+
+    @ex(help='Inserts a new song to the chord dht', 
         # sub-command level arguments. ex: 'chordy command1 --foo bar'
         arguments=[
             ### add a sample foo option under subcommand namespace
-            ( [ '-f', '--foo' ],
-              { 'help' : 'notorious foo option',
-                'action'  : 'store',
-                'dest' : 'foo' } ),
+            ( [ '-k', '--key' ],
+              { 'help' : 'song name',
+                'dest' : 'key' } ),
+            ( [ '-v', '--value'],
+              {
+                'help' : 'song value',
+                'dest' : 'value'
+              })
         ],
     )
-    def show(self):
-        """Example sub-command."""
-
-        data = {
-            'foo' : 'bar',
-        }
-
-        ### do something with arguments
-        if self.app.pargs.foo is not None:
-            data['foo'] = self.app.pargs.foo
-
-        self.app.render(data, 'command1.jinja2')
-
-    @ex(help='Inserts a new song to the chord dht')
-    def insert(self, song):
+    def insert(self):
+        key = self.app.pargs.key
+        value = self.app.pargs.value
+        # print(self.app.pargs)
         with grpc.insecure_channel('localhost:1024') as channel:
             stub = client_services_pb2_grpc.ClientServiceStub(channel)
-            response = stub.Insert(client_services_pb2.InsertRequest(song="mplas"))
+            response = stub.Insert(client_services_pb2.InsertRequest(song=key, value=value))
+            print(response)
 
-    @ex(help='Query a song from the network')
+    @ex(help='Query a song from the network',
+        # sub-command level arguments. ex: 'chordy command1 --foo bar'
+        arguments=[
+            ### add a sample foo option under subcommand namespace
+            ( [ '-k', '--key' ],
+              { 'help' : 'song name',
+                'dest' : 'key' } )
+        ],
+    )
     def query(self):
-        print('Inside query')
+        key = self.app.pargs.key
+        with grpc.insecure_channel('localhost:1024') as channel:
+            stub = client_services_pb2_grpc.ClientServiceStub(channel)
+            response = stub.Query(client_services_pb2.QueryRequest(song=key))
+            print(response)
 
-    @ex(help='Delete a song from the network')
+    @ex(help='Delete a song from the network',
+        # sub-command level arguments. ex: 'chordy command1 --foo bar'
+        arguments=[
+            ### add a sample foo option under subcommand namespace
+            ( [ '-k', '--key' ],
+              { 'help' : 'song name',
+                'dest' : 'key' } )
+        ],
+    )
     def delete(self):
         print('Inside query')
     
     @ex(help='Display the topology of the network')
     def overlay(self):
+        with grpc.insecure_channel('localhost:1024') as channel:
+            stub = client_services_pb2_grpc.ClientServiceStub(channel)
+            response = stub.overlay(client_services_pb2.OverlayRequest())
+            print(response)
         print('I')
 
     @ex(help='Leave the chord network')
-    def depart(self, id):
+    def depart(self):
         print('K')
