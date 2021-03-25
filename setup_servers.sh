0bae6d2f03090cd9154214c6aa1df833286bce8f
Giannis Stergiou <giannissterg@hotmail.com>
2021-03-25 06:32:19 +0200

    [scripts] Adapted script for all remote machines

--- setup_servers.sh
+++ setup_servers.sh
@@ -4,8 +4,13 @@
 SESSION="Server"
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
-IP=$(ifconfig eth2 | grep "inet " | awk '{print $2}')
-PORT=$1
+PORT=$2
+if [ "$PORT" = "1024"]
+then
+    IP=$(ifconfig eth2 | grep "inet " | awk '{print $2}')
+else
+    IP=$(ifconfig eth1 | grep "inet " | awk '{print $2}')
+fi
 
 # Only create tmux session if it doesn't already exist
 if [ "$SESSIONEXISTS" = "" ]
@@ -18,7 +23,7 @@ then
     tmux send-keys -t 'Server1' "python3 server/main.py $IP $PORT" C-m
 
     sleep 1
-    
+
     # Create and setup pane for server 2
     tmux new-window -t $SESSION:1 -n 'Server2'
     tmux send-keys -t 'Server2' "python3 server/main.py $IP $((PORT + 1))" C-m
